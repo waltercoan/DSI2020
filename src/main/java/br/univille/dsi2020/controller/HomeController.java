680f07cd2ccab0a45e5d626697e862f9cf9071d9
Walter Coan <waltercoan@hotmail.com>
2020-06-09 23:02:56 +0000

    09062020 mapeamento classe paciente

---
+++ src/main/java/br/univille/dsi2020/controller/HomeController.java
@@ -0,0 +1,23 @@
+package br.univille.dsi2020.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+    
+    @GetMapping({"/index",""})
+    public ModelAndView index(){
+        return new ModelAndView("home/index","titulo","Título da aplicação");
+    }    
+    @GetMapping("/contato")
+    @ResponseBody
+    public String contato(){
+        return "walter.s@univille.br";
+    }
+}
