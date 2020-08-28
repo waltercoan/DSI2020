0577b156485d2dffb0fb75516684b64cbdec7e03
Walter Coan <waltercoan@hotmail.com>
2020-08-28 01:09:41 +0000

    conclusão da implementacao da API

--- src/main/java/br/univille/dsi2020/api/PacienteControllerAPI.java
+++ src/main/java/br/univille/dsi2020/api/PacienteControllerAPI.java
@@ -4,8 +4,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import br.univille.dsi2020.model.Paciente;
@@ -27,5 +31,29 @@ public class PacienteControllerAPI {
     public ResponseEntity<Paciente> getById(@PathVariable("id") Paciente paciente){
         return new ResponseEntity<>(paciente, HttpStatus.OK);
     }
+
+    @PostMapping
+    public ResponseEntity<Paciente> save (@RequestBody Paciente paciente){
+        service.save(paciente);
+        return new ResponseEntity<>(paciente, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Paciente> update(@PathVariable("id") Paciente pacienteAntigo, 
+                                           @RequestBody Paciente pacienteAtualizado){
+        pacienteAntigo.setNome(pacienteAtualizado.getNome());
+        pacienteAntigo.setSexo(pacienteAtualizado.getSexo());
+        pacienteAntigo.setDataNascimento(pacienteAtualizado.getDataNascimento());
+        service.save(pacienteAntigo);
+        return new ResponseEntity<>(pacienteAntigo, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Paciente> delete(@PathVariable("id") Paciente paciente){
+        service.delete(paciente);
+        return new ResponseEntity<>(paciente, HttpStatus.OK);
+    }
+    
+
 
 }
